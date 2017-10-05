b54780888de9e7aa4747d84e8284db62c879e713
starfish01 <patrick.labes@gmail.com>
2017-10-05 02:52:53 +0000

    second Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,17 @@
 var game;
 
 // Create a new game instance 600px wide and 450 px tall
-game= new Phaser.Game(600,450, Phaser.AUTO,'');
+game = new Phaser.Game(600,450, Phaser.AUTO,'');
 
 //first parameter is how out state will be called
 //second parameter is an object containing the needed methods for state functionality
 game.state.add('Menu',Menu);
 game.state.start('Menu');
 
+
 //adding the game state
 game.state.add('Game',Game);
 
 game.state.start('Menu');
+
+game.state.add('Game_Over', Game_Over);
